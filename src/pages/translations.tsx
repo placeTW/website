3ea04eb5f812495d75517177519a2e3d6f21404c
Howard Tseng <howardt12345@gmail.com>
2023-07-28 22:32:49 -0400

    cleanup

--- src/pages/translations.tsx
+++ src/pages/translations.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { supabase } from "../supabase";
 import { Session } from "@supabase/supabase-js";
 import TranslationAuth from "../modules/translation-auth";
