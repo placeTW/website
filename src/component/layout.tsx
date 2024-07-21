37ae1012a6cf0e0e7da4495f2920f6a0c132e27d
milspectacular <milspec.ppau@gmail.com>
2024-07-21 18:56:36 +0000

    deleted duplicate translations in the json, removed translation from layout.tsx. its not used.

--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -2,11 +2,8 @@ import { Box } from "@chakra-ui/react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import GlobalUserStatusListener from './global-user-status-listener';
-import { useTranslation } from 'react-i18next';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { t } = useTranslation();
-
   return (
     <Box w="100vw">
       <GlobalUserStatusListener>
