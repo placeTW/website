bdc8a5d9c329cd2880ef78a30f87f74d8a6c97fa
Howard Tseng <howardt12345@gmail.com>
2024-07-30 19:45:09 -0400

    Revert "updating style of eye icon to match others"
    This reverts commit 2b4eee5c1c1cdee517cb4583b8422e3330222479.

--- src/component/art-card.tsx
+++ src/component/art-card.tsx
@@ -58,6 +58,8 @@ const ArtCard: FC<ArtCardProps> = ({ artPiece }) => {
               position="absolute"
               top="5px"
               left="5px"
+              background="white"
+              color="blue.500"
             />
             <Image
               alt={artPiece.layer_name}
