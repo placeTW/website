c7fccfdbab1ad58c4e5d295950ef497a5c117d1a
Howard Tseng <howardt12345@gmail.com>
2024-07-30 23:47:43 -0400

    update dimensions on window grow

--- src/component/art_tool/viewport.tsx
+++ src/component/art_tool/viewport.tsx
@@ -84,12 +84,18 @@ const Viewport: React.FC = () => {
   // We cant set the h & w on Stage to 100% it only takes px values so we have to
   // find the parent container's w and h and then manually set those !
   useEffect(() => {
-    if (divRef.current?.offsetHeight && divRef.current?.offsetWidth) {
-      setDimensions({
-        width: divRef.current.offsetWidth,
-        height: divRef.current.offsetHeight,
-      });
-    }
+    if (!divRef.current) return;
+    const resizeObserver = new ResizeObserver(() => {
+      // Do what you want to do when the size of the element changes
+      if (divRef.current?.offsetHeight && divRef.current?.offsetWidth) {
+        setDimensions({
+          width: divRef.current.offsetWidth,
+          height: divRef.current.offsetHeight,
+        });
+      }
+    });
+    resizeObserver.observe(divRef.current);
+    return () => resizeObserver.disconnect(); // clean up
   }, []);
 
   const handleWheel = (e: Konva.KonvaEventObject<WheelEvent>) => {
@@ -195,7 +201,7 @@ const Viewport: React.FC = () => {
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
-        height: "calc(100vh - 80px)",
+        height: "100%",
       }}
       ref={divRef}
     >
