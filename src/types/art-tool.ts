4dbe9da833f892a8570369022d622d4f18dccd57
milspectacular <milspec.ppau@gmail.com>
2024-08-01 18:08:49 +0000

    added pixel data type

--- src/types/art-tool.ts
+++ src/types/art-tool.ts
@@ -8,3 +8,12 @@ export interface DesignInfo {
   rank_name: string; // Ensure this property is included
   liked_by: string[];
 }
+
+// Define and export the Pixel interface
+export interface Pixel {
+  id: number;
+  x: number;
+  y: number;
+  color: string;
+  canvas: string;
+}
