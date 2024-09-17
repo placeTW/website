8007c55b448fc998fc24b2b5ac5d142911d72779
howardt12345 <howardt12345@gmail.com>
2024-09-17 00:39:42 -0400

    fix: env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
 import react from "@vitejs/plugin-react";
 import sass from "sass";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          implementation: sass,
+        },
       },
     },
-  },
-  publicDir: "public",
-  server: {
-    hmr: true,
-  },
-  build: {
-    target: "esnext",
-  },
-  base: process.env.NODE_ENV === "development" && import.meta.env?.VITE_CODE_SERVER ? "/absproxy/5173" : "/",
+    publicDir: "public",
+    server: {
+      hmr: true,
+    },
+    build: {
+      target: "esnext",
+    },
+    base:
+      process.env.NODE_ENV === "development" &&
+      env?.VITE_CODE_SERVER
+        ? "/absproxy/5173"
+        : "/",
+  };
 });
