a72ac29abb34c7e0fb6d5d6321de02887939c26f
Howard Tseng <howardt12345@gmail.com>
2025-08-24 16:28:04 -0400

    usability concerns with smaller screens

--- src/utils/pngUtils.ts
+++ src/utils/pngUtils.ts
@@ -142,13 +142,13 @@ export const calculateColorDistance = (color1: string, color2: string): number =
   const hex1 = color1.replace('#', '');
   const hex2 = color2.replace('#', '');
   
-  const r1 = parseInt(hex1.substr(0, 2), 16);
-  const g1 = parseInt(hex1.substr(2, 2), 16);
-  const b1 = parseInt(hex1.substr(4, 2), 16);
+  const r1 = parseInt(hex1.substring(0, 2), 16);
+  const g1 = parseInt(hex1.substring(2, 4), 16);
+  const b1 = parseInt(hex1.substring(4, 6), 16);
   
-  const r2 = parseInt(hex2.substr(0, 2), 16);
-  const g2 = parseInt(hex2.substr(2, 2), 16);
-  const b2 = parseInt(hex2.substr(4, 2), 16);
+  const r2 = parseInt(hex2.substring(0, 2), 16);
+  const g2 = parseInt(hex2.substring(2, 4), 16);
+  const b2 = parseInt(hex2.substring(4, 6), 16);
   
   return Math.sqrt(
     Math.pow(r2 - r1, 2) + 
